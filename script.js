adc10b13f86e1515ccafc1182fe9032b0c4a7700
matifelitti <matiasfelitti@gmail.com>
2024-12-06 23:28:23 -0300

    adding buttons html and css, changing info ubication

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
-function changeBackgroundColor(color) {
-  document.body.style.backgroundColor = color;
+let currentProduct = 0;
+
+const products = document.querySelectorAll(".product");
+const totalProducts = products.length;
+const productSlider = document.querySelector(".product-slider");
+
+function navigate(direction) {
+  if (direction === "next") {
+    currentProduct = (currentProduct + 1) % totalProducts;
+  } else if (direction === "prev") {
+    currentProduct = (currentProduct - 1 + totalProducts) % totalProducts;
+  }
+
+  updateSlider();
+}
+
+function updateSlider() {
+  const offset = -currentProduct * products[0].offsetWidth;
+  productSlider.style.transform = `translateX(${offset}px)`;
+
+  const productColor = products[currentProduct].getAttribute("data-color");
+  document.body.style.backgroundColor = productColor;
 }
